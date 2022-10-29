a3e755a1e641cb2d11d11fe7aa96a6105fc8c3d7
Alexander <a.kireev.qa@gmail.com>
2022-10-29 23:46:53 +0300

    feat: добавить schema.sql, репозиторий и работу с ним для сущности User

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,13 +1,18 @@
 package ru.practicum.shareit.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import javax.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
+@JsonInclude(Include.NON_NULL)
 public class UserDto {
 
   private Long id;
