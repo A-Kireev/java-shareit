8ebd3cc3709637a0075772b56eea0d9e6342981c
Alexander <a.kireev.qa@gmail.com>
2022-11-27 21:47:06 +0300

    test: добавить инт тесты для Item

--- src/test/java/ru/practicum/shareit/integration/ItemTests.java
+++ src/test/java/ru/practicum/shareit/integration/ItemTests.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.integration;
 
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -14,6 +15,7 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 @SpringBootTest
@@ -51,6 +53,87 @@ class ItemTests {
     assertSoftly(softAssertions ->
         softAssertions.assertThat(targetItems.size())
             .isEqualTo(sourceItems.size()));
+  }
+
+  @Test
+  void getItemTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    em.persist(user);
+
+    var sourceItem = Item.builder()
+        .name("itemName")
+        .description("itemDescription")
+        .isAvailable(true)
+        .ownerId(user.getId())
+        .build();
+    em.persist(sourceItem);
+    em.flush();
+
+    var targetItem = itemService.getItem(user.getId(), sourceItem.getId());
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItem)
+            .usingRecursiveComparison()
+            .ignoringFields("id", "lastBooking", "nextBooking", "comments")
+            .isEqualTo(sourceItem));
+  }
+
+  @Test
+  void searchItemsTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    var sourceItems = List.of(
+        ItemDto.builder()
+            .name("itemName")
+            .description("itemDescription")
+            .isAvailable(true)
+            .build(),
+        ItemDto.builder()
+            .name("secondItemName")
+            .description("secondItemDescription")
+            .isAvailable(true)
+            .build()
+    );
+
+    em.persist(user);
+    for (var item : sourceItems) {
+      var entity = ItemMapper.toItem(item, user.getId());
+      em.persist(entity);
+    }
+    em.flush();
+
+    var targetItems = itemService.searchItem("itemName", null, null);
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItems.size())
+            .isEqualTo(sourceItems.size()));
+  }
+
+  @Test
+  void findItemByRequestIdTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    em.persist(user);
+
+    var request = ItemRequest.builder()
+        .description("requestDescription")
+        .createDateTime(LocalDateTime.now())
+        .requesterId(user.getId())
+        .build();
+    em.persist(request);
+
+    var sourceItem = Item.builder()
+        .name("itemName")
+        .description("itemDescription")
+        .isAvailable(true)
+        .ownerId(user.getId())
+        .requestId(request.getId())
+        .build();
+    em.persist(sourceItem);
+    em.flush();
+
+    var targetItems = itemService.findItemByRequestId(sourceItem.getRequestId());
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItems.get(0))
+            .usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(sourceItem));
   }
 
   @Test
