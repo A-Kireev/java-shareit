7d176cd66bbbf4f6780c1a6cd3c412b53f20f8dd
Alexander <a.kireev.qa@gmail.com>
2022-11-27 22:38:00 +0300

    test: добавить инт тесты для ItemRequests

--- src/test/java/ru/practicum/shareit/integration/ItemRequestsTests.java
+++ src/test/java/ru/practicum/shareit/integration/ItemRequestsTests.java
@@ -24,6 +24,29 @@ class ItemRequestsTests {
   private final ItemRequestService itemRequestService;
 
   @Test
+  void createItemRequestTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    var notOwnerUser = new User(null, "notOwnerName", "mail1@mail.com");
+    var sourceItemRequest = ItemRequestDto.builder()
+        .description("itemRequestDescription")
+        .created(LocalDateTime.now())
+        .build();
+
+    em.persist(user);
+    em.persist(notOwnerUser);
+    var entity = ItemRequestMapper.toItemRequest(user.getId(), sourceItemRequest);
+    em.persist(entity);
+    em.flush();
+
+    var targetItemRequests = itemRequestService.createItemRequest(notOwnerUser.getId(), sourceItemRequest);
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItemRequests)
+            .usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(sourceItemRequest));
+  }
+
+  @Test
   void getItemRequestsTest() {
     var user = new User(null, "authorName", "mail@mail.com");
     var notOwnerUser = new User(null, "notOwnerName", "mail1@mail.com");
@@ -50,5 +73,90 @@ class ItemRequestsTests {
     assertSoftly(softAssertions ->
         softAssertions.assertThat(targetItemRequests.size())
             .isEqualTo(sourceItemRequests.size()));
+  }
+
+  @Test
+  void getItemRequestsPageTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    var notOwnerUser = new User(null, "notOwnerName", "mail1@mail.com");
+    var sourceItemRequests = List.of(
+        ItemRequestDto.builder()
+            .description("itemRequestDescription")
+            .created(LocalDateTime.now())
+            .build(),
+        ItemRequestDto.builder()
+            .description("secondItemRequestDescription")
+            .created(LocalDateTime.now())
+            .build()
+    );
+
+    em.persist(user);
+    em.persist(notOwnerUser);
+    for (var item : sourceItemRequests) {
+      var entity = ItemRequestMapper.toItemRequest(user.getId(), item);
+      em.persist(entity);
+    }
+    em.flush();
+
+    var targetItemRequests = itemRequestService.getItemRequests(notOwnerUser.getId(), 0, 2);
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItemRequests.size())
+            .isEqualTo(sourceItemRequests.size()));
+  }
+
+  @Test
+  void getUserItemRequestsTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    var notOwnerUser = new User(null, "notOwnerName", "mail1@mail.com");
+    var sourceItemRequests = List.of(
+        ItemRequestDto.builder()
+            .description("itemRequestDescription")
+            .created(LocalDateTime.now())
+            .build(),
+        ItemRequestDto.builder()
+            .description("secondItemRequestDescription")
+            .created(LocalDateTime.now())
+            .build()
+    );
+
+    em.persist(user);
+    em.persist(notOwnerUser);
+    for (var item : sourceItemRequests) {
+      var entity = ItemRequestMapper.toItemRequest(user.getId(), item);
+      em.persist(entity);
+    }
+    em.flush();
+
+    var targetItemRequests = itemRequestService.getUserItemRequests(user.getId());
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItemRequests.size())
+            .isEqualTo(sourceItemRequests.size()));
+  }
+
+  @Test
+  void getItemRequestTest() {
+    var user = new User(null, "authorName", "mail@mail.com");
+    var notOwnerUser = new User(null, "notOwnerName", "mail1@mail.com");
+    var sourceItemRequests = List.of(
+        ItemRequestDto.builder()
+            .description("itemRequestDescription")
+            .created(LocalDateTime.now())
+            .build()
+    );
+
+    em.persist(user);
+    em.persist(notOwnerUser);
+    for (var item : sourceItemRequests) {
+      var entity = ItemRequestMapper.toItemRequest(user.getId(), item);
+      em.persist(entity);
+    }
+    em.flush();
+
+    var targetItemRequests = itemRequestService.getItemRequest(user.getId(), 1L);
+    assertSoftly(softAssertions ->
+        softAssertions.assertThat(targetItemRequests)
+            .usingRecursiveComparison()
+            .ignoringFields("id", "items")
+            .isEqualTo(sourceItemRequests.get(0)));
   }
 }
